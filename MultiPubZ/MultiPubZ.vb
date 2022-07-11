49ef68d803a1626ec117045575d728bdc453fc4f
Kelvin Dixon <madbnz@msn.com>
2022-07-12 08:46:57 +1200

    PubToMatch added

--- MultiPubZ/MultiPubZ.vb
+++ MultiPubZ/MultiPubZ.vb
@@ -9,6 +9,7 @@ Module MultiPubZ
             Dim MatchedFolder As String = My.Settings.MatchedFolder + "\"
             Dim Publist() = My.Settings.PubList.Split(",")
             Dim DaysToAdd As Integer = My.Settings.DaysToAdd
+            Dim PubToMatch As String = My.Settings.PubToMatch
             Dim DateFormat As String = "yyyyMMdd"
             Dim SrcFile As String = InputFolder + TestFile
             Dim DstFile As String
@@ -20,28 +21,35 @@ Module MultiPubZ
             'Console.WriteLine(testFileName)
             If FileIO.FileSystem.FileExists(SrcFile) Then 'Is this a real File?
                 Dim Result = Regex.Match(TestFile, "(?<Publication>.+)-(?<PubDate>\d{4}\d{2}\d{2})-(?<Section>Z)(?<Pages>\d{2}).pdf", RegexOptions.IgnoreCase)
-                If Result.Success Then ' Filename Matches Pattern and
+                If Result.Success Then ' Filename Matches Pattern
                     'Console.WriteLine("Filename Matches")
                     Dim Publication = Result.Groups.Item("Publication").Value
-                    Dim CurrentDate = Result.Groups.Item("PubDate").Value
-                    Dim FutureDate = Date.ParseExact(CurrentDate, DateFormat, Globalization.CultureInfo.InvariantCulture).AddDays(DaysToAdd).ToString(DateFormat)
-                    Dim Section = Result.Groups.Item("Section").Value
-                    Dim Pages = Result.Groups.Item("Pages").Value
-                    Dim NewName As String = ""
-                    'Console.WriteLine("Publication: {0}", Publication)
-                    'Console.WriteLine("CurrentDate: {0}", CurrentDate)
-                    'Console.WriteLine("FutureDate: {0}", FutureDate)
-                    'Console.WriteLine("Section: {0}", Section)
-                    'Console.WriteLine("Pages: {0}", Pages)
-                    For Each Pub As String In Publist
-                        NewName = Pub + "-" + FutureDate + "-" + Section + Pages + ".pdf"
-                        DstFile = MatchedFolder + NewName
-                        'Console.WriteLine("Copyto: {0}", DstFile)
-                        FileIO.FileSystem.CopyFile(SrcFile, DstFile, True)
-                    Next
-                    DstFile = MatchedFolder + TestFile
-                    'Console.WriteLine("Moveto: {0}", DstFile)
-                    FileIO.FileSystem.MoveFile(SrcFile, DstFile, True)
+                    If Publication = PubToMatch Then
+                        Dim CurrentDate = Result.Groups.Item("PubDate").Value
+                        Dim FutureDate = Date.ParseExact(CurrentDate, DateFormat, Globalization.CultureInfo.InvariantCulture).AddDays(DaysToAdd).ToString(DateFormat)
+                        Dim Section = Result.Groups.Item("Section").Value
+                        Dim Pages = Result.Groups.Item("Pages").Value
+                        Dim NewName As String = ""
+                        'Console.WriteLine("Publication: {0}", Publication)
+                        'Console.WriteLine("CurrentDate: {0}", CurrentDate)
+                        'Console.WriteLine("FutureDate: {0}", FutureDate)
+                        'Console.WriteLine("Section: {0}", Section)
+                        'Console.WriteLine("Pages: {0}", Pages)
+                        For Each Pub As String In Publist
+                            NewName = Pub + "-" + FutureDate + "-" + Section + Pages + ".pdf"
+                            DstFile = MatchedFolder + NewName
+                            'Console.WriteLine("Copyto: {0}", DstFile)
+                            FileIO.FileSystem.CopyFile(SrcFile, DstFile, True)
+                        Next
+                        DstFile = MatchedFolder + TestFile
+                        'Console.WriteLine("Moveto: {0}", DstFile)
+                        FileIO.FileSystem.MoveFile(SrcFile, DstFile, True)
+                    Else
+                        'Console.WriteLine("Wrong Publication")
+                        DstFile = ErrorFolder + TestFile
+                        'Console.WriteLine("Moveto: {0}", DstFile)
+                        FileIO.FileSystem.MoveFile(SrcFile, DstFile, True)
+                    End If
                 Else
                     'Console.WriteLine("Filename Does Not Match")
                     DstFile = ErrorFolder + TestFile
--- MultiPubZ/My Project/Settings.Designer.vb
+++ MultiPubZ/My Project/Settings.Designer.vb
@@ -15,7 +15,7 @@ Option Explicit On
 Namespace My
     
     <Global.System.Runtime.CompilerServices.CompilerGeneratedAttribute(),  _
-     Global.System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.10.0.0"),  _
+     Global.System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.2.0.0"),  _
      Global.System.ComponentModel.EditorBrowsableAttribute(Global.System.ComponentModel.EditorBrowsableState.Advanced)>  _
     Partial Friend NotInheritable Class MySettings
         Inherits Global.System.Configuration.ApplicationSettingsBase
@@ -111,6 +111,18 @@ Namespace My
             End Get
             Set
                 Me("DaysToAdd") = value
+            End Set
+        End Property
+        
+        <Global.System.Configuration.UserScopedSettingAttribute(),  _
+         Global.System.Diagnostics.DebuggerNonUserCodeAttribute(),  _
+         Global.System.Configuration.DefaultSettingValueAttribute("SUSED1")>  _
+        Public Property PubToMatch() As String
+            Get
+                Return CType(Me("PubToMatch"),String)
+            End Get
+            Set
+                Me("PubToMatch") = value
             End Set
         End Property
     End Class
